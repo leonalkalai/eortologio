a8c5624f768e579c67cdff70eb2fb0db69937e54
Leon Kountouras <koundouras@gmail.com>
2021-05-17 09:42:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,25 +2,35 @@
 
 document.querySelector('body').style.backgroundColor = "orange";
 
-let info = document.querySelector('#info');
-let name = "Λέων";
-function giortes(){
-    setTimeout(() => {
-        let giorti = `το όνομα που γιορτάζει σήμερα είναι: ${name}`;
-        return giorti;
-    }, 1000);   
-}
-info.innerHTML = giortes()
+// let info = document.querySelector('#info');
+// let name = "Λέων";
+// function giortes(){
+//     setTimeout(() => {
+//         let giorti = `το όνομα που γιορτάζει σήμερα είναι: ${name}`;
+//         return giorti;
+//     }, 1000);   
+// }
+// info.innerHTML = giortes()
 
 
-jQuery.ajax({
-  url: "http://www.eortologio.gr/rss/si_el.xml",
-  method: "GET",
-  headers: {
+// jQuery.ajax({
+//   url: "http://www.eortologio.gr/rss/si_el.xml",
+//   method: "GET",
+//   headers: {
 
-  }
-}).then(response => {
-    console.log(response);
-}).catch(error => {
-    console.log(error);
+//   }
+// }).then(response => {
+//     console.log(response);
+// }).catch(error => {
+//     console.log(error);
+// })
+
+// Array to store the list of urls to fetch, max 50 urls at one time
+  var arr = ["https://developer.mozilla.org/en-US/docs/Web/API/Request","https://en.wikipedia.org/"]
+  // Paste the url which you got in step 4
+ fetch('https://tiny-grass-88fc.leonalkalai.workers.dev',{
+  method: 'POST',
+  body: JSON.stringify(arr)
 })
+.then(response => response.text())
+.then(data => console.log(data))
