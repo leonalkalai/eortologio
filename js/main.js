0db45f0a3cb78541bf99b808d3b35e923cef4f0b
Leon Kountouras <koundouras@gmail.com>
2021-05-17 10:45:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,25 +50,9 @@ $.ajax(RSS_URL, {
   dataType: "xml",
 
   success: function(data) {
-    $(data)
-      .find("item")
-      .each(function() {
-        const el = $(this);
 
-        const template = `
-          <article>
-            <img src="${el.find("link").text()}/image/large.png" alt="">
-            <h2>
-              <a href="${el
-                .find("link")
-                .text()}" target="_blank" rel="noopener">
-                ${el.find("title").text()}
-              </a>
-            </h2>
-          </article>
-        `;
-
-        document.body.insertAdjacentHTML("beforeend", template);
+      console.log(data)
+    
       });
   }
 });
